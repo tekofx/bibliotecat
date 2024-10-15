621f6d5ac07683ffb4f89637b4fd6211f09cf478
tekofx <tekofxx@gmail.com>
2024-10-15 22:02:33 +0200

    Renamed

--- app/src/main/java/dev/tekofx/biblioteques/BibliotecasResponse.kt
+++ app/src/main/java/dev/tekofx/biblioteques/BibliotecasResponse.kt
@@ -2,7 +2,7 @@ package dev.tekofx.biblioteques.ui.home
 
 import com.google.gson.annotations.SerializedName
 
-data class BibliotecasReponse (
+data class BibliotecasResponse (
     @SerializedName("status") var status: String,
     @SerializedName("elements") var elements: ArrayList<Biblioteca> = arrayListOf()
 )
