5f671e148b52d65638bdfb6a9437310a99527eed
tekofx <tekofxx@gmail.com>
2024-10-27 17:30:19 +0100

    Rearranged code

--- app/src/main/java/dev/tekofx/biblioteques/MainActivity.kt
+++ app/src/main/java/dev/tekofx/biblioteques/MainActivity.kt
@@ -13,12 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import dev.tekofx.biblioteques.components.BottomNavigation
-import dev.tekofx.biblioteques.databinding.ActivityMainBinding
 import dev.tekofx.biblioteques.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,21 +31,6 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        val navView: BottomNavigationView = binding.navView
-//
-//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
     }
 }
 
