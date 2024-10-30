bbc63701bfdb21a2e1aa6dc9b322caf83212d54e
tekofx <tekofxx@gmail.com>
2024-10-31 00:27:32 +0100

    Separated OpeningStatus

--- app/src/main/java/dev/tekofx/biblioteques/components/LibraryItem.kt
+++ app/src/main/java/dev/tekofx/biblioteques/components/LibraryItem.kt
@@ -1,6 +1,5 @@
 package dev.tekofx.biblioteques.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,31 +17,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
-import dev.tekofx.biblioteques.R
 import dev.tekofx.biblioteques.model.Library
 import dev.tekofx.biblioteques.model.LibraryDummy
-import java.time.LocalDate
-import java.time.LocalTime
 
 @Composable
 fun LibraryItem(library: Library) {
-    val date = LocalDate.now()
-    val time = LocalTime.now()
-    var color = R.color.red_closed
-    if (library.isOpen(date, time)) {
-        color = R.color.green_open
-        if (library.isClosingSoon(date, time)) {
-            color = R.color.yellow_soon
-        }
-    }
+
 
     Surface(tonalElevation = 40.dp, shape = RoundedCornerShape(20.dp)) {
 
@@ -74,20 +59,7 @@ fun LibraryItem(library: Library) {
                     )
                     Text(text = library.municipiNom, modifier = Modifier.padding(top = 8.dp))
                 }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.padding(top = 8.dp)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.circle),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(20.dp)
-                            .padding(end = 8.dp),
-                        colorFilter = ColorFilter.tint(colorResource(id = color))
-                    )
-                    Text(text = library.generateStateMessage(date, time), fontSize = 14.sp)
-                }
+                OpeningStatus(library)
 
             }
         }
