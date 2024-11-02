84a1579dbf2441f51b27f4019756c5564b196917
tekofx <tekofxx@gmail.com>
2024-11-02 22:40:17 +0100

    Added spacing and increase size

--- app/src/main/java/dev/tekofx/biblioteques/components/library/LibraryContactCard.kt
+++ app/src/main/java/dev/tekofx/biblioteques/components/library/LibraryContactCard.kt
@@ -1,15 +1,20 @@
 package dev.tekofx.biblioteques.components.library
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.tekofx.biblioteques.ui.IconResource
 
 @Composable
@@ -19,9 +24,19 @@ fun LibraryContactCard(iconResource: IconResource, text: String) {
         tonalElevation = 20.dp,
         shape = RoundedCornerShape(10.dp)
     ) {
-        Row(modifier = Modifier.padding(10.dp)) {
-            Icon(iconResource.asPainterResource(), text)
-            Text(text = text)
+        Row(
+            modifier = Modifier.padding(10.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                iconResource.asPainterResource(),
+                text,
+                modifier = Modifier
+                    .width(50.dp)
+                    .height(50.dp)
+            )
+            Text(text = text, fontSize = 20.sp)
         }
     }
 }
