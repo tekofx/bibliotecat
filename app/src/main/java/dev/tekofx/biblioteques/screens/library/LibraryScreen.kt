b83a74f28eeceb7c441cc68fe26569cdff9d0f02
tekofx <tekofxx@gmail.com>
2024-11-03 14:36:39 +0100

    Centered loader

--- app/src/main/java/dev/tekofx/biblioteques/screens/library/LibraryScreen.kt
+++ app/src/main/java/dev/tekofx/biblioteques/screens/library/LibraryScreen.kt
@@ -3,6 +3,7 @@ package dev.tekofx.biblioteques.screens.library
 
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -101,7 +103,15 @@ fun LibraryScreen(
         }
     } ?: run {
         if (isLoading) {
-            CircularProgressIndicator()
+            Box(
+                modifier = Modifier.fillMaxSize(),
+
+                ) {
+
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
         } else {
 
             Text(text = "No library selected")
