f00e1eae4465cd6763588a221eff3cb75f1e0737
tekofx <tekofxx@gmail.com>
2024-11-13 21:28:44 +0100

    Reformat routes

--- app/src/main/java/dev/tekofx/biblioteques/navigation/NavigateDestinations.kt
+++ app/src/main/java/dev/tekofx/biblioteques/navigation/NavigateDestinations.kt
@@ -2,8 +2,8 @@ package dev.tekofx.biblioteques.navigation
 
 object NavigateDestinations {
     const val LIBRARIES_ROUTE = "LibrariesScreen"
-    const val LIBRARY_DETAILS_ROUTE = "LibreriesScreen/library"
+    const val LIBRARY_DETAILS_ROUTE = "$LIBRARIES_ROUTE/library"
     const val BOOK_SEARCH_ROUTE = "BooksScreen"
-    const val BOOK_RESULTS_ROUTE = "BooksScreen/search"
-    const val BOOK_DETAILS_ROUTE = "BooksScree/book"
+    const val BOOK_RESULTS_ROUTE = "$BOOK_SEARCH_ROUTE/search"
+    const val BOOK_DETAILS_ROUTE = "$BOOK_SEARCH_ROUTE/book"
 }
