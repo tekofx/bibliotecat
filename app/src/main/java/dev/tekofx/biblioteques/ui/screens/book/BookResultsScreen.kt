ba32b19bb28159580f65aaa9b14223c143899e63
tekofx <tekofxx@gmail.com>
2024-11-18 17:19:57 +0100

    Moved BookCard to BookResultsScreen

--- app/src/main/java/dev/tekofx/biblioteques/ui/screens/book/BookResultsScreen.kt
+++ app/src/main/java/dev/tekofx/biblioteques/ui/screens/book/BookResultsScreen.kt
@@ -2,9 +2,15 @@ package dev.tekofx.biblioteques.ui.screens.book
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -17,9 +23,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil3.compose.AsyncImage
 import dev.tekofx.biblioteques.model.BookResult
 import dev.tekofx.biblioteques.model.BookResults
 import dev.tekofx.biblioteques.model.EmptyResults
@@ -27,7 +37,7 @@ import dev.tekofx.biblioteques.model.GeneralResults
 import dev.tekofx.biblioteques.model.SearchResult
 import dev.tekofx.biblioteques.navigation.NavigateDestinations
 import dev.tekofx.biblioteques.ui.components.PaginatedList
-import dev.tekofx.biblioteques.ui.components.book.BookCard
+import dev.tekofx.biblioteques.ui.theme.Typography
 import dev.tekofx.biblioteques.ui.viewModels.BookViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -98,6 +108,63 @@ fun BookResultsScreen(
 
     }
 }
+
+@Composable
+fun BookCard(
+    book: BookResult,
+    onClick: () -> Unit
+) {
+
+    Surface(
+        tonalElevation = 40.dp,
+        shape = RoundedCornerShape(20.dp),
+        onClick = { onClick() }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(10.dp)
+        ) {
+
+            Row(
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                AsyncImage(
+                    model = book.image,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(200.dp)
+                        .aspectRatio(0.6f)
+                        .clip(RoundedCornerShape(10.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Column(
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    Text(
+                        text = book.title,
+                        style = Typography.titleLarge
+                    )
+                    Text(
+                        text = book.author,
+                        style = Typography.titleMedium
+                    )
+                    book.publication?.let {
+                        Text(
+                            text = it,
+                            style = Typography.titleMedium
+                        )
+                    }
+                }
+            }
+
+        }
+    }
+}
+
 
 @Composable
 fun GeneralSearchResultCard(
