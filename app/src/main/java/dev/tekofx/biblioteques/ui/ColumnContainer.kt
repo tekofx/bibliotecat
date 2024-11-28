788ab2f7c81c9b56b8635192193f9ceae0e054b7
tekofx <tekofxx@gmail.com>
2024-11-28 16:58:55 +0100

    Created ColumnContainer

---
+++ app/src/main/java/dev/tekofx/biblioteques/ui/ColumnContainer.kt
@@ -0,0 +1,26 @@
+package dev.tekofx.biblioteques.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun ColumnContainer(
+
+    content: @Composable() (ColumnScope.() -> Unit)
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        content()
+    }
+}
