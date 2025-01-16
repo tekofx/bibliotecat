c95ea13ff59a225c2a4a3091a6b99bc10dd2ad21
tekofx <tekofxx@gmail.com>
2025-01-16 19:06:04 +0100

    Aligned vertically

--- app/src/main/java/dev/tekofx/biblioteques/ui/components/library/LibraryCard.kt
+++ app/src/main/java/dev/tekofx/biblioteques/ui/components/library/LibraryCard.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
@@ -40,6 +41,7 @@ fun LibraryCard(
 
         Row(
             modifier = Modifier
+                .height(150.dp)
                 .fillMaxWidth()
                 .padding(10.dp),
             verticalAlignment = Alignment.CenterVertically,
@@ -60,7 +62,7 @@ fun LibraryCard(
                 modifier = Modifier
                     .fillMaxHeight()
                     .weight(2f),
-                verticalArrangement = Arrangement.Top
+                verticalArrangement = Arrangement.SpaceBetween
             ) {
 
                 Text(
