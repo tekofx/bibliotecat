34a6bc4785fe43d32a0cac0091f0073331306473
tekofx <tekofxx@gmail.com>
2025-01-31 20:01:49 +0100

    Changed Text Style when no libraries

--- app/src/main/java/dev/tekofx/biblioteques/ui/components/library/LibraryList.kt
+++ app/src/main/java/dev/tekofx/biblioteques/ui/components/library/LibraryList.kt
@@ -11,11 +11,14 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dev.tekofx.biblioteques.model.library.Library
 import dev.tekofx.biblioteques.ui.components.animations.SlideDirection
 import dev.tekofx.biblioteques.ui.components.animations.SlideVertically
+import dev.tekofx.biblioteques.ui.theme.Typography
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -35,17 +38,23 @@ fun LibraryList(
         visible = !isLoading,
         SlideDirection.UP,
     ) {
-        if (libraries.isEmpty() && filtersApplied) {
-            Text("No hi ha biblioteques amb aquestes filtres")
-        }
-
         LazyColumn(
             state = listState,
             modifier = Modifier
                 .fillMaxSize()
                 .padding(top = 10.dp),
-            verticalArrangement = Arrangement.spacedBy(10.dp)
+            verticalArrangement = Arrangement.spacedBy(10.dp,Alignment.CenterVertically)
         ) {
+            item{
+                if (libraries.isEmpty() && filtersApplied) {
+                    Text(
+                        text = "No hi ha biblioteques amb aquestes filtres",
+                        modifier = Modifier.padding(16.dp),
+                        style = Typography.headlineLarge,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
             items(libraries, key = { it.id }) { library ->
                 Row(modifier = Modifier.animateItem()) {
                     LibraryCard(
