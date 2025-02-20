12e9fd69a3873de07d44fa58e5057e2b4480fd02
tekofx <tekofxx@gmail.com>
2025-02-20 17:56:26 +0100

    Minor changes

--- app/src/main/java/dev/tekofx/biblioteques/ui/screens/library/LibrariesScreen.kt
+++ app/src/main/java/dev/tekofx/biblioteques/ui/screens/library/LibrariesScreen.kt
@@ -38,10 +38,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
@@ -79,13 +76,13 @@ fun LibrariesScreen(
     val showOnlyOpenTest by libraryViewModel.showOnlyOpen.collectAsState()
     val filtersApplied by libraryViewModel.filtersApplied.collectAsState()
 
-    var showBottomSheet by remember { mutableStateOf(false) }
-
     // Loaders
     val isLoading by libraryViewModel.isLoading.collectAsState()
 
     // Errors
     val errorMessage by libraryViewModel.errorMessage.observeAsState("")
+
+    // Bottom Sheet
     val scope = rememberCoroutineScope()
     val scaffoldState = rememberBottomSheetScaffoldState(
         bottomSheetState = rememberStandardBottomSheetState(
