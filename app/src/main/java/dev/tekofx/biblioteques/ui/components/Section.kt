594b28a495c0f4e9abe5316288d996e01aa7c8cd
tekofx <tekofxx@gmail.com>
2025-02-21 12:28:09 +0100

    Padding adjustments

--- app/src/main/java/dev/tekofx/biblioteques/ui/components/Section.kt
+++ app/src/main/java/dev/tekofx/biblioteques/ui/components/Section.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.tekofx.biblioteques.ui.IconResource
 
@@ -23,11 +27,12 @@ fun Section(title: String, description: String, iconResource: IconResource) {
         shape = MaterialTheme.shapes.extraLarge
     ) {
         Row(
-            modifier = Modifier.padding(10.dp),
-            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier.padding(vertical = 10.dp, horizontal = 20.dp),
+            horizontalArrangement = Arrangement.spacedBy(20.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Icon(
+                modifier = Modifier.size(30.dp),
                 painter = iconResource.asPainterResource(),
                 contentDescription = ""
             )
@@ -39,4 +44,14 @@ fun Section(title: String, description: String, iconResource: IconResource) {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun SectionPreview() {
+    Section(
+        "Title",
+        "Description",
+        IconResource.fromImageVector(Icons.Outlined.AccountCircle)
+    )
 }
