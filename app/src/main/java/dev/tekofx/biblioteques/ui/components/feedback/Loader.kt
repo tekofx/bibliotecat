e653db9e4ec09de0ee30da1e9f9f8353e132ce26
tekofx <tekofxx@gmail.com>
2025-06-10 14:36:34 +0200

    Added background

--- app/src/main/java/dev/tekofx/biblioteques/ui/components/feedback/Loader.kt
+++ app/src/main/java/dev/tekofx/biblioteques/ui/components/feedback/Loader.kt
@@ -1,14 +1,21 @@
 package dev.tekofx.biblioteques.ui.components.feedback
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 
@@ -17,21 +24,33 @@ fun Loader(
     isLoading: Boolean,
     text: String,
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .zIndex(100f)
-            .padding(horizontal = 10.dp)
-    ) {
-        if (isLoading) {
-            Column(
-                modifier = Modifier.align(Alignment.Center),
-                horizontalAlignment = Alignment.CenterHorizontally
+    if (isLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .zIndex(100f)
+                .padding(horizontal = 10.dp)
+        ) {
+            Card(
+                modifier = Modifier.align(Alignment.Center)
             ) {
-                CircularProgressIndicator()
-                Text(text = text)
+                Column(
+                    modifier = Modifier
+                        .padding(30.dp)
+                        .wrapContentSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    CircularProgressIndicator()
+                    Text(text = text)
+                }
             }
         }
-
     }
+}
+
+@Composable
+@Preview
+fun LoaderPreview(){
+    Loader(true, "loading")
 }
